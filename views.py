b133c7afdc3ff4d7c4f8f0e430e5641e57c3c6c6
Seungmin Ham <hamsm3184@naver.com>
2024-03-19 22:19:18 +0900

    Fix : 타임라인 텍스트 수정

--- views.py
+++ views.py
@@ -91,7 +91,7 @@ def index2(request):
     ]
 
     # 본인 api key 삽입
-    genai.configure(api_key="AIzaSyB842rnY66Om_-2SwSnh-R98c7v_OWiB9Q")
+    genai.configure(api_key="")
     model = genai.GenerativeModel('gemini-pro', safety_settings=safety_settings)
     with open(f'script_{final_link[0]}.txt', "r", encoding='UTF8') as f:
         example = f.read()
