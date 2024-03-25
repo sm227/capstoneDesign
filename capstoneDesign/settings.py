7cb240c175090af1b04e23500b6992e229c08f2e
Seungmin Ham <hamsm3184@gmail.com>
2024-03-26 01:46:09 +0900

    Feat: dockerfile 추가

--- capstoneDesign/settings.py
+++ capstoneDesign/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-tt*1!y_!rq*2^il!*v_i1ro-7_wcy^9xif%th1*fw&a2asa#rl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 # Application definition
 
--- summary.py
+++ summary.py
@@ -1,14 +1,5 @@
 # single_turn.py
 def test1():
 
-    import google.generativeai as genai
-    import os
-    genai.configure(api_key="AIzaSyB842rnY66Om_-2SwSnh-R98c7v_OWiB9Q")
-    model = genai.GenerativeModel('gemini-pro')
-    with open("example.txt", "r", encoding='UTF8') as f:
-        example = f.read()
-
-
-    response = model.generate_content(example)
-    print(response.text)
+    print("test")
 test1()
