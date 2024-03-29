37fdb6ba1d9f1edf44a3844055bdab7b9b6c8bed
Seungmin Ham <hamsm3184@naver.com>
2024-03-29 17:56:03 +0900

    Feat: 회원가입 기능 추가

--- common/views.py
+++ common/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 
-from django.contrib.auth import logout
-from django.shortcuts import redirect
+from django.contrib.auth import logout, authenticate, login
+from django.shortcuts import redirect, render
+from common.forms import UserForm
+
 
 def logout_view(request):
     logout(request)
@@ -9,3 +11,16 @@ def logout_view(request):
 
 
 # Create your views here.
+def signup(request):
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)  # 사용자 인증
+            login(request, user)  # 로그인
+            return redirect('common:login')
+    else:
+        form = UserForm()
+    return render(request, 'common/signup.html', {'form': form})
