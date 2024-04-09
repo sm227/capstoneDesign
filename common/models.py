cc56619884bbea5c835fd6a5f7fdb16694358cea
kimbh9806 <kimbh9806@naver.com>
2024-04-10 03:08:59 +0900

    csrf_token 에러, 회원가입 폼 수정

--- common/models.py
+++ common/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+class User(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.username
 # Create your models here.
