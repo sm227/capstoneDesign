78225579669125e28fd927ae136ee82d75c5a42d
ddabongdochi <162669229+ddabongdochi@users.noreply.github.com>
2024-05-15 21:47:44 +0900

    remove txt, json

--- views.py
+++ views.py
@@ -167,10 +167,6 @@ def index2(request):
     response = model.generate_content(example)
     # response = model.generate_content("보기 좋게 요약해줘.", example)
     
-    #remove script and summery files from os
-    os.remove(f'script_{final_link[0]}.txt')
-    os.remove(f'script_{final_link[0]}.json')
-    
 
     # print(response.text)
     a = "<h1>aa</h1>"
