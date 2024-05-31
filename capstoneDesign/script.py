380492ca88d9da308d8c8a967ce3e91daca337d8
kimbh9806 <kimbh9806@naver.com>
2024-05-31 16:53:23 +0900

    전체삭제 버튼 위치 변경 요약 언어 영어, 브리티쉬 추가

--- capstoneDesign/script.py
+++ capstoneDesign/script.py
@@ -3,7 +3,7 @@ import json
 
 def download_script_json(id):
     video_id = id # 예시 한글 아이디: vDXDAKh8eyo, 영어 아이디: MIgmwSaJ2eo
-    transcript = YouTubeTranscriptApi.get_transcript(video_id, languages=['ko', 'en'])
+    transcript = YouTubeTranscriptApi.get_transcript(video_id, languages=['ko', 'en', 'en-GB'])
     # 자막 데이터를 JSON 파일로 저장
     with open(f'script_{video_id}.json', 'w', encoding='utf-8') as json_file:
         json.dump(transcript, json_file, ensure_ascii=False, indent=4)
