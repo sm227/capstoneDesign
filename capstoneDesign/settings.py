6173878da42fccb6b80a940341f3ee1e68ecd8b2
Seungmin Ham <99596895+sm227@users.noreply.github.com>
2024-06-07 00:54:36 +0000

    .

--- capstoneDesign/settings.py
+++ capstoneDesign/settings.py
@@ -142,8 +142,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'capstoneDesign','static')]
 
-STATIC_ROOT = os.path.join(BASE_DIR,'capstoneDesign', 'static')
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR,'capstoneDesign', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
